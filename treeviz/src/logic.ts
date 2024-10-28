b88b89b36a93baa3b04e4eb8a79c61d5f10900b2
rhit-lardnece <lardnece@rose-hulman.edu>
2024-10-28 15:37:38 -0400

    csv things

--- treeviz/src/logic.ts
+++ treeviz/src/logic.ts
@@ -2,3 +2,27 @@
 // function to create the tree structure
 // function to export data for each node
 
+import path, { parse } from "path";
+
+type Node = {
+    numNodes: number;
+    name: string;
+    weight: number;
+    root: Node;
+    fromNodes: Node[];
+    toNodes: Node[];
+}
+
+// read csv
+const fs = require("fs");
+const csv = require("csv-parser");
+
+export function readCSV(file: string) {
+    const csvFilePath = path.resolve(__dirname, 'treeExanple.csv');
+
+  const headers = [];
+
+  const fileContent = fs.readFileSync(csvFilePath, { encoding: 'utf-8' });
+
+  
+}
